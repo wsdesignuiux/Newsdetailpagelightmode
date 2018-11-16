9d65063020a62dfbeee017784d1c62f6e344aebc
Rushabh Patel <wsdesignuiux@gmail.com>
2018-11-16 14:53:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/wolfsoft3/newsdetailpagelightmode/News_detail_page_lightmode.java
@@ -0,0 +1,13 @@
+package com.example.wolfsoft3.newsdetailpagelightmode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class News_detail_page_lightmode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detail_page_lightmode);
+    }
+}
